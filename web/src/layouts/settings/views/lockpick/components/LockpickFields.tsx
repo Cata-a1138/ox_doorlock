b984ba2c2a5ab1292f7349e6512e1be54508391f
Cata_a <3478600437@qq.com>
2023-09-28 02:08:34 +0800

    翻译设置页面

--- web/src/layouts/settings/views/lockpick/components/LockpickFields.tsx
+++ web/src/layouts/settings/views/lockpick/components/LockpickFields.tsx
@@ -3,13 +3,15 @@ import { ActionIcon, Group, Modal, Select, Tooltip } from '@mantine/core';
 import { useState } from 'react';
 import { TbSettings, TbTrash } from 'react-icons/tb';
 import DifficultyModal from '../../characters/components/DifficultyModal';
+import { useLocales } from '../../../../../providers/LocaleProvider';
 
+const { locale } = useLocales();
 const selectData: { label: string; value: string }[] = [
-  { label: 'Easy', value: 'easy' },
-  { label: 'Medium', value: 'medium' },
-  { label: 'Hard', value: 'hard' },
-  { label: 'Custom', value: 'custom' },
-];
+  { label: locale.ui.easy, value: 'easy' },
+  { label: locale.ui.medium, value: 'medium' },
+  { label: locale.ui.hard, value: 'hard' },
+  { label: locale.ui.custom, value: 'custom' },
+]
 
 const LockpickFields: React.FC = () => {
   const lockpickFields = useStore((state) => state.lockpickDifficulty);
@@ -34,15 +36,15 @@ const LockpickFields: React.FC = () => {
             data={selectData}
             value={typeof field === 'string' ? field : 'custom'}
             readOnly
-            placeholder="Edit row to select value"
+            placeholder={locale.ui.edit_info}
             sx={{ width: '80%' }}
           />
-          <Tooltip label="Edit row">
+          <Tooltip label={locale.ui.edit}>
             <ActionIcon color="blue.4" variant="transparent" onClick={() => setModal({ opened: true, index })}>
               <TbSettings size={24} />
             </ActionIcon>
           </Tooltip>
-          <Tooltip label="Delete row">
+          <Tooltip label={locale.ui.delete}>
             <ActionIcon color="red.4" variant="transparent" onClick={() => handleRowDelete(index)}>
               <TbTrash size={24} />
             </ActionIcon>
@@ -53,7 +55,7 @@ const LockpickFields: React.FC = () => {
         opened={modal.opened}
         onClose={() => setModal({ ...modal, opened: false })}
         transition="fade"
-        title="Lockpick difficulty"
+        title={locale.ui.lockpick_difficulty}
         centered
         size="xs"
         withCloseButton={false}
